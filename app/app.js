daab434835f763145c4a0e6b6e2db5a16d8f309d
ZachParris <zacharyjparris@gmail.com>
2016-06-14 18:09:03 -0500

    basic setup and basic auth

--- app/app.js
+++ app/app.js
@@ -1,1 +1,77 @@
-console.log("hi thar");
+"use strict";
+
+var app = angular.module('PitstopApp', ["ngRoute"])
+  .constant("firebaseURL", "https://pitstop-app-zp.firebaseio.com/");
+
+  let isAuth = (AuthFactory) => new Promise((resolve, reject) => {
+
+  if (AuthFactory.isAuthenticated()) {
+    console.log("User is authenticated.");
+    resolve();
+  } else {
+    console.log("User is not authenticated.");
+    reject();
+  }
+
+});
+
+
+app.config(function($routeProvider) {
+  $routeProvider.
+  when('/', {
+    templateUrl: 'partials/clientList.html',
+    controller: "ClientListCtrl",
+    resolve: {
+      isAuth
+    }
+  }).
+  when('/', {
+    templateUrl: 'partials/clientList.html',
+    controller: "ClientListCtrl",
+    resolve: {
+      isAuth
+    }
+  }).
+  when('/add-client', {
+    templateUrl: 'partials/addClient.html',
+    controller: "AddClient",
+    // resolve: {
+    //   // isAuth
+    // }
+  }).
+  when('/intake', {
+    templateUrl: 'partials/intakeView.html',
+    controller: "WatchListCtrl",
+    // resolve: {
+    //   // isAuth
+    // }
+  }).
+  when('/details', {
+    templateUrl: 'partials/clientDetails.html',
+    controller: "Ctrl",
+    // resolve: {
+    //   // isAuth
+    // }
+  }).
+  when('/login', {
+    templateUrl: 'partials/login.html',
+    controller: "LoginCtrl"
+  }).
+  when('/logout', {
+    templateUrl: 'partials/login.html',
+    controller: "LoginCtrl"
+  }).
+  otherwise('/');
+});
+
+app.run(($location) => {
+
+  let watchlistRef = new Firebase("https://pitstop-app-zp.firebaseio.com/");
+  watchlistRef.unauth();
+
+  watchlistRef.onAuth(authData => {
+    if (!authData) {
+      $location.path("/login");
+    }
+  })
+  });
